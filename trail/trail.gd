2d457dd43f9244335304fc3491acac549fdd2198
Sytonk <122539345+SytonK@users.noreply.github.com>
2024-07-21 19:16:59 +0300

    add frequency

--- trail/trail.gd
+++ trail/trail.gd
@@ -4,8 +4,21 @@ extends Line2D
 
 @export var max_length: int
 var queue : Array
+
+@export var frequency: float = 0
+var time: float = 0
+
  
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
+	if frequency > 0:
+		time += delta
+		if time >= frequency:
+			time -= frequency
+			_update_points()
+	else:
+		_update_points()
+
+func _update_points() -> void:
 	var pos = _get_position()
 	
 	queue.push_front(pos)
